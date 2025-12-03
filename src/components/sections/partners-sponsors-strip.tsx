4fd537861d3be9609eb58c32c0db71350e6c1144
derispewss <derisfirmansyah177@gmail.com>
2025-12-03 23:19:54 +0700

    feat: add partners and sponsors section with a marquee display and include all associated logo assets.

--- src/components/sections/partners-sponsors-strip.tsx
+++ src/components/sections/partners-sponsors-strip.tsx
@@ -11,10 +11,19 @@ import logoPartner5 from "@/assets/media-partner/Logo (Dengan teks) 1.png";
 import logoPartner6 from "@/assets/media-partner/Logo HMIF UNSIQ 1.png";
 import logoPartner7 from "@/assets/media-partner/Logo OmahTI (white) 1.png";
 import logoPartner8 from "@/assets/media-partner/logo HMDTI 1.png";
+import logoPartner9 from "@/assets/media-partner/BEM-FIK 1.png";
+import logoPartner10 from "@/assets/media-partner/IMG_0982 1.png";
+import logoPartner11 from "@/assets/media-partner/logo dncc 1.png";
 
 // Import your sponsor logos here
 import logoSponsor1 from "@/assets/sponsor/IDCloudHost-Logo-vertivcal-03 3 (1).png";
 import logoSponsor2 from "@/assets/sponsor/Logo Seminar Kit 1 (1).png";
+import logoSponsor3 from "@/assets/sponsor/Bank_x0020_Jateng_x0020_Logo.cdr.png";
+import logoSponsor4 from "@/assets/sponsor/LOGO DOKTER LAPTOP SPONSOR UDINUS_page-0001 1.png";
+import logoSponsor5 from "@/assets/sponsor/Salinan Dicoding Logo - White 3.png";
+import logoSponsor6 from "@/assets/sponsor/WhatsApp Image 2025-12-02 at 17.34.12_899613dc 1.png";
+import logoSponsor7 from "@/assets/sponsor/logo clowor bulet merah 2.png";
+import logoSponsor8 from "@/assets/sponsor/download 2.png";
 
 const mediaPartnerLogos: LogoItem[] = [
   { src: logoPartner1, alt: "Media Partner 1" },
@@ -25,11 +34,20 @@ const mediaPartnerLogos: LogoItem[] = [
   { src: logoPartner6, alt: "Media Partner 6" },
   { src: logoPartner7, alt: "Media Partner 7" },
   { src: logoPartner8, alt: "Media Partner 8" },
+  { src: logoPartner9, alt: "Media Partner 9" },
+  { src: logoPartner10, alt: "Media Partner 10" },
+  { src: logoPartner11, alt: "Media Partner 11" },
 ];
 
 const sponsorLogos: LogoItem[] = [
   { src: logoSponsor1, alt: "Sponsor 1" },
   { src: logoSponsor2, alt: "Sponsor 2" },
+  { src: logoSponsor3, alt: "Sponsor 3" },
+  { src: logoSponsor4, alt: "Sponsor 4" },
+  { src: logoSponsor5, alt: "Sponsor 5" },
+  { src: logoSponsor6, alt: "Sponsor 6" },
+  { src: logoSponsor7, alt: "Sponsor 7" },
+  { src: logoSponsor8, alt: "Sponsor 8" },
 ];
 
 export default function PartnersSponsorsStrip() {
